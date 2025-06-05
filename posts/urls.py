593231e4d86a739aa135435167dbccd943482086
Nar Always <nareshsathi79@gmail.com>
2025-06-04 23:16:19 -0400

    update asignment

--- posts/urls.py
+++ posts/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create/", views.PostCreateView.as_view(), name="create"),
-    path("edit/", views.PostEditView.as_view(), name="edit"),
-    path("delete/", views.PostDeleteView.as_view(), name="delete"),
+    path("", views.PostListView.as_view(), name="list"),
+    path("drafts/", views.DraftPostListView.as_view(), name="drafts"),
+    path("archive/", views.ArchivedPostListView.as_view(), name="archive"),
+    path("new/", views.PostCreateView.as_view(), name="new"),
+    path("<int:pk>/detail", views.PostDetailView.as_view(), name="detail"),
+    path("<int:pk>/edit/", views.PostEditView.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
     path("update/", views.PostUpdateView.as_view(), name="update"),
-    path("list/", views.PostListView.as_view(), name="list"),
-    path("detail", views.PostDetailView.as_view(), name="detail")
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,9 +4,8 @@ from django.views.generic import (
     CreateView,
     UpdateView,
     DeleteView,
-    
-    
 )
+from .models import Post, Status
 from .models import Post
 from django.urls import reverse_lazy, reverse
 
@@ -50,19 +49,76 @@ class PostListView(ListView):
     template_name = "posts/list.html"
     model = Post
 
-class PostEditView(UpdateView):
+    def get_context_data(self,  **kwargs):
+        context = super().get_context_data(**kwargs)
+        published = Status.objects.get(name="published")
+        context["title"] = "Published"
+        context["post_list"] = (
+            Post.objects
+            .filter(status=published)
+            .order_by("created_on").reverse()
+        )
+        return context
+
+class ArchivedPostListView(LoginRequiredMixin, ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_context_data(self, **kwrgs):
+        context = super().get_context_data(**kwrgs)
+        archived = Status.objects.get(name="archived")
+        context["title"] = "Archived"
+        context["post_list"] = (
+            Post.objects
+            .filter(status=archived)
+            .order_by("created_on").reverse()
+        )
+        return context
+
+class DraftPostListView(LoginRequiredMixin, ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        draft = Status.objects.get(name="draft")
+        context["title"] = "Draft"
+        context["post_list"] = (
+            Post.objects
+            .filter(status=draft)
+            .filter(author=self.request.user)
+            .order_by("created_on").reverse()
+        )
+        return context
+
+class PostEditView(UserPassesTestMixin, UpdateView):
     template_name = "posts/edit.html"
     fields = ["title", "subtitle", "body", "author", "status"]
     model = Post
 
-class PostDetailView(DetailView):
+    def test_func(self):
+        post = self.get_object()
+        if post.status.name == "published":
+            return True
+        elif post.status.name == "archived" and self.request.user.is_authenticated:
+            return True
+        else:
+            return False
+
+
+class PostDetailView(UserPassesTestMixin, DetailView):
     template_name = "posts/detail.html"
     model = Post
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-
+    def test_func(self):
+        Post = self.get_object()
+        if post.status.name == "published":
+            return True 
+        elif post.status.name == "archieved" and self.request.user.is_authenticated:
+            return True
+        elif post.status.name == "draft" and self.request.user == post.author:
+            return True
+        else:
+            return False
 
 
